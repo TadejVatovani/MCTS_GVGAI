c8303a434edf9feff13f83710b89ff8156c1e59d
TadejVatovani <vatko1995@gmail.com>
2016-07-30 18:31:29 +0200

    Add files via upload
    Final version

--- Agent.java
+++ Agent.java
@@ -15,8 +15,9 @@ public class Agent extends AbstractPlayer {
     public static Types.ACTIONS[] actions;
 
     Node root;
-    PlayerOL player = new PlayerOL();
-
+    //PlayerOL player = new PlayerOL();
+    Player player = new Player();
+    
     public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) {
         ArrayList<Types.ACTIONS> act = so.getAvailableActions();
         actions = new Types.ACTIONS[act.size()];
@@ -29,11 +30,19 @@ public class Agent extends AbstractPlayer {
     public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         int action = 0;
         ArrayList<Observation> obs[] = stateObs.getFromAvatarSpritesPositions();
-        ArrayList<Observation> grid[][] = stateObs.getObservationGrid();
+        ArrayList<Observation> grid[][] =
+        stateObs.getObservationGrid();
 
         //select the action
-        action = player.actforgetn(stateObs, elapsedTimer, 3);
-
+        //action = player.act1(stateObs, elapsedTimer);
+        //action = player.actforgetn(stateObs, elapsedTimer, 2);
+        //action = player.actforgetn(stateObs, elapsedTimer, 3);
+        //action = player.actforgetn(stateObs, elapsedTimer, 5);
+        action = player.actforgetn(stateObs, elapsedTimer, 7);
+        
+        
+        
+        
         //if the returned action is -1 do nothing,since it's still building a tree 
         if (action == -1) {
             return Types.ACTIONS.ACTION_NIL;
--- PlayerOL.java
+++ PlayerOL.java
@@ -9,40 +9,8 @@ public class PlayerOL {
 
     Random rnd = new Random();
 
-    /**
-     * Makes a tree for n rounds then acts out, sets the root as the previous
-     * action(so it keeps a portion of the tree).This concept didn't really work
-     * in the end: either it just keeps on making a bigger tree (which means it
-     * throws 1 depth away but keeps n-1,which I think becomes too outdated) or
-     * it tries to build from the initial tree,which loses information too fast
-     * so it just becomes an act1
-     */
     int counter = 0;
     NodeOL root = null;
-
-    public int actremembern(StateObservation stateObs, ElapsedCpuTimer elapsedTimer, int n) {
-        elapsedTimer.setMaxTimeMillis(elapsedTimer.remainingTimeMillis());
-        NodeOL.setRootState(stateObs);
-
-        if (counter == 0) {
-            root = new NodeOL(null, rnd);
-            counter++;
-            return -1;
-        } else if (counter < n) {
-            root.expandTree(elapsedTimer);
-            counter++;
-            return -1;
-        } else {
-            int action = root.search(elapsedTimer);
-            root = root.children[action];
-            root.parent = null;
-            counter = 1;
-
-            return action;
-        }
-
-    }
-
     /**
      * Makes a tree for n rounds then acts out, after it acts out it forgets the
      * tree
@@ -52,6 +20,7 @@ public class PlayerOL {
         NodeOL.setRootState(stateObs);
         if (counter == 0) {
             root = new NodeOL(null, rnd);
+            root.expandTree(elapsedTimer);
             counter++;
             return -1;
         } else if (counter < n) {
